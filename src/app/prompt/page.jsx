9047c4d8e24771a91a7f3c11f1ed0de0743d5f94
Victor-Sin <80609577+Victor-Sin@users.noreply.github.com>
2024-03-21 14:51:05 +0100

    Adding route prompt img

---
+++ src/app/prompt/page.jsx
@@ -0,0 +1,36 @@
+"use client" // This directive is unusual and typically not standard JavaScript or React. Make sure it is necessary for your project.
+
+import { useState } from "react";
+import styles from "./prompt.module.scss";
+
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+export default function Prompt() {
+  const [input, setInput] = useState("");
+  const [url, setUrl] = useState(null);
+
+  function createImg(){
+    fetch(`${apiUrl}/image/post/create`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        prompt: input,
+      })
+    })
+        .then(response => response.json())
+        .then(data => {
+          setUrl(data.url);
+            console.log(data.url)
+        });
+  }
+
+  return (
+      <>
+        <textarea type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="Ecrivez votre message" />
+        <button onClick={createImg}>Envoyer</button>
+        <img className={styles.img} src={url} alt="image" />
+      </>
+  );
+}
