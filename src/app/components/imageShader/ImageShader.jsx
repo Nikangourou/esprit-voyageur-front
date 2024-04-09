a48da5eb51d914c767ede92643cbc8ac6851e29f
Victor-Sin <80609577+Victor-Sin@users.noreply.github.com>
2024-04-09 17:42:35 +0200

    Add shader water

--- src/app/components/imageShader/ImageShader.jsx
+++ src/app/components/imageShader/ImageShader.jsx
@@ -1,49 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import { Curtains, Plane } from "curtainsjs";
 import styles from "./ImageShader.module.scss";
-
-export const vertexShader = `
-    precision mediump float;
-    
-    attribute vec3 aVertexPosition;
-    attribute vec2 aTextureCoord;
-    
-    uniform mat4 uMVMatrix;
-    uniform mat4 uPMatrix;
-    
-    uniform mat4 uTextureMatrix0;
-    
-    varying vec3 vVertexPosition;
-    varying vec2 vTextureCoord;
-    
-    void main() {
-        gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
-        
-        // varyings
-        vVertexPosition = aVertexPosition;
-        vTextureCoord = (uTextureMatrix0 * vec4(aTextureCoord, 0.0, 1.0)).xy;
-    }
-`;
-
-export const fragmentShader = `
-     precision mediump float;
-
-    varying vec3 vVertexPosition;
-    varying vec2 vTextureCoord;
-    
-    uniform sampler2D uSampler0;
-    
-    uniform float uTime;
-    
-    void main() {
-        vec2 textureCoord = vTextureCoord;
-        // displace our pixels along the X axis based on our time uniform
-        // textures coords are ranging from 0.0 to 1.0 on both axis
-        textureCoord.x += sin(textureCoord.y * 25.0) * cos(textureCoord.x * 25.0) * (cos(uTime / 50.0)) / 25.0;
-        
-        gl_FragColor = texture2D(uSampler0, textureCoord);
-    }
-`;
+import { fragmentShader, vertexShader } from "./shader/shader";
 
 export default function ImageShader({ url }) {
   const canvasRef = useRef();
@@ -80,7 +38,8 @@ export default function ImageShader({ url }) {
     <div className="imageShader">
       <div id={styles.canvas} ref={canvasRef}></div>
       <div className={styles.plane} ref={planeElementRef}>
-        <img src={url} crossOrigin="Access-Control-Allow-Origin" />
+        <img src={"/image.png"} crossOrigin="Access-Control-Allow-Origin" />
+        <img src={"/voronoi.jpg"} crossOrigin="Access-Control-Allow-Origin" />
       </div>
     </div>
   );
---
+++ src/app/components/imageShader/shader/shader.js
@@ -0,0 +1,256 @@
+const functionsNoise = `
+ float cnoise(vec3 P){
+        vec3 Pi0 = floor(P); // Integer part for indexing
+        vec3 Pi1 = Pi0 + vec3(1.0); // Integer part + 1
+        Pi0 = mod(Pi0, 289.0);
+        Pi1 = mod(Pi1, 289.0);
+        vec3 Pf0 = fract(P); // Fractional part for interpolation
+        vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+        vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+        vec4 iy = vec4(Pi0.yy, Pi1.yy);
+        vec4 iz0 = Pi0.zzzz;
+        vec4 iz1 = Pi1.zzzz;
+    
+        vec4 ixy = permute(permute(ix) + iy);
+        vec4 ixy0 = permute(ixy + iz0);
+        vec4 ixy1 = permute(ixy + iz1);
+    
+        vec4 gx0 = ixy0 / 7.0;
+        vec4 gy0 = fract(floor(gx0) / 7.0) - 0.5;
+        gx0 = fract(gx0);
+        vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+        vec4 sz0 = step(gz0, vec4(0.0));
+        gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+        gy0 -= sz0 * (step(0.0, gy0) - 0.5);
+    
+        vec4 gx1 = ixy1 / 7.0;
+        vec4 gy1 = fract(floor(gx1) / 7.0) - 0.5;
+        gx1 = fract(gx1);
+        vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+        vec4 sz1 = step(gz1, vec4(0.0));
+        gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+        gy1 -= sz1 * (step(0.0, gy1) - 0.5);
+    
+        vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
+        vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
+        vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
+        vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
+        vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
+        vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
+        vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
+        vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
+    
+        vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+        g000 *= norm0.x;
+        g010 *= norm0.y;
+        g100 *= norm0.z;
+        g110 *= norm0.w;
+        vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+        g001 *= norm1.x;
+        g011 *= norm1.y;
+        g101 *= norm1.z;
+        g111 *= norm1.w;
+    
+        float n000 = dot(g000, Pf0);
+        float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+        float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+        float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+        float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+        float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+        float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+        float n111 = dot(g111, Pf1);
+    
+        vec3 fade_xyz = fade(Pf0);
+        vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+        vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+        float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x);
+        return 2.2 * n_xyz;
+    }
+    
+    float fbm(vec3 x) {
+        float v = 0.0;
+        float a = 0.5;
+        vec3 shift = vec3(100);
+        for (int i = 0; i < 2; ++i) {
+            v += a * cnoise(x);
+            x = x * 2.0 + shift;
+            a *= 0.5;
+        }
+        return v;
+    }   
+`;
+
+const functionsVoronoi = `
+
+
+vec2 hash2(vec2 p)
+{
+       // Dave Hoskin's hash as in https://www.shadertoy.com/view/4djSRW
+       vec3 p3 = fract(vec3(p.xyx) * vec3(.1031, .1030, .0973));
+       p3 += dot(p3, p3.yzx+19.19);
+       vec2 o = fract(vec2((p3.x + p3.y)*p3.z, (p3.x+p3.z)*p3.y));
+
+    #ifdef ANIMATE
+       o = 0.5 + 0.5*sin( uTime*0.0001 + o*6.2831853 );
+    #endif
+   return o;
+}
+
+
+//---------------------------------------------------------------
+// 4x4 scan in both passes = most accurate
+//---------------------------------------------------------------
+
+vec3 voronoi( in vec2 x )
+{
+#if 1
+    // slower, but better handles big numbers
+    vec2 n = floor(x);
+    vec2 f = fract(x);
+    vec2 h = step(.5,f) - 2.;
+    n += h; f -= h;
+#else
+    vec2 n = floor(x - 1.5);
+    vec2 f = x - n;
+#endif
+
+    //----------------------------------
+    // first pass: regular voronoi
+    //----------------------------------
+ vec2 mr;
+
+    float md = 8.0;
+    for( int j=0; j<=3; j++ )
+    for( int i=0; i<=3; i++ )
+    {
+        vec2 g = vec2(float(i),float(j));
+        vec2 o = hash2( n + g );
+        vec2 r = g + o - f;
+        float d = dot(r,r);
+
+        if( d<md )
+        {
+            md = d;
+            mr = r;
+        }
+    }
+
+    //----------------------------------
+    // second pass: distance to borders
+    //----------------------------------
+    md = 8.0;
+    for( int j=0; j<=3; j++ )
+    for( int i=0; i<=3; i++ )
+    {
+        vec2 g = vec2(float(i),float(j));
+        vec2 o = hash2( n + g );
+        vec2 r = g + o - f;
+
+        if( dot(mr-r,mr-r)>EPSILON ) // skip the same cell
+        md = min( md, dot( 0.5*(mr+r), normalize(r-mr) ) );
+    }
+
+    return vec3( md, mr );
+}
+
+vec3 plot( vec2 p, float ss )
+{
+    vec3 c = voronoi( p*2.66 );
+    
+    float d = length(c.yz);
+    float n = c.r * (1.- d);
+    float e = step(.02,n);
+    vec3 col = vec3(e);
+    col = smoothstep(col,vec3(.05), vec3(.0));
+    return col;
+}
+
+`;
+
+export const fragmentShader = `
+    precision highp float;
+    
+    #define SECOND_PASS 2
+
+    #define ANIMATE
+    
+    // How far cells can go off center during animation (must be <= .5)
+    #define ANIMATE_D .5
+    
+    // Points cannot be closer than sqrt(EPSILON)
+    #define EPSILON .00001
+
+    varying vec3 vVertexPosition;
+    varying vec2 vTextureCoord;
+    varying vec2 vTextureCoord1;
+
+    uniform sampler2D uSampler0;
+    uniform sampler2D uSampler1;
+    
+    uniform float uTime;
+
+    //Classic Perlin 3D Noise 
+    //by Stefan Gustavson
+    //
+    vec4 permute(vec4 x){return mod(((x*34.0)+1.0)*x, 289.0);}
+    vec4 taylorInvSqrt(vec4 r){return 1.79284291400159 - 0.85373472095314 * r;}
+    vec3 fade(vec3 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
+    
+    ${functionsNoise}
+    
+    ${functionsVoronoi} 
+    
+    void main() {
+        vec2 textureCoord = (vTextureCoord-.5)*1.15+.5 ;
+        vec2 textureCoord1 = (vTextureCoord1-.5)*1.15+.5;
+        
+        float borderNoise = fbm(vec3(textureCoord*7.5,uTime*.0015) ) * 5.5;
+        float innerNoise =  fbm(vec3(textureCoord1*3.5,uTime*.00175) ) * 35.5;
+        
+        vec4 text =  texture2D(uSampler0, textureCoord + vec2(innerNoise*.001));
+        vec4 textVoronoi =  texture2D(uSampler1, textureCoord1);
+
+   
+        // Apply border noise
+        textureCoord = textureCoord + vec2(borderNoise) * .025;
+        vec2 tmpUv = vec2(textureCoord1.x ,textureCoord1.y) ;
+
+        float circle = distance(textureCoord,vec2(0.5));
+        circle = 1. - step(.44,circle);
+        
+        
+        vec3 voronoi = plot(tmpUv + innerNoise*.0075,0.001);
+        vec4 finalText = text *( circle *  vec4(voronoi,voronoi.x) );
+        
+        
+        
+        gl_FragColor = finalText   ;
+        // gl_FragColor = finalText * (innerNoise)  ;
+    }
+`;
+
+export const vertexShader = `
+    precision mediump float;
+    
+    attribute vec3 aVertexPosition;
+    attribute vec2 aTextureCoord;
+    
+    uniform mat4 uMVMatrix;
+    uniform mat4 uPMatrix;
+    
+    uniform mat4 uTextureMatrix0;
+    uniform mat4 uTextureMatrix1;
+    
+    varying vec3 vVertexPosition;
+    varying vec2 vTextureCoord;
+    varying vec2 vTextureCoord1;
+    
+    void main() {
+        gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
+        
+        // varyings
+        vVertexPosition = aVertexPosition;
+        vTextureCoord = (uTextureMatrix0 * vec4(aTextureCoord, 0.0, 1.0)).xy;
+        vTextureCoord1 = (uTextureMatrix1 * vec4(aTextureCoord, 0.0, 1.0)).xy;
+    }
+`;
