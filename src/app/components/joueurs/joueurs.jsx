2131aa37ceef1ed9e1324b4ea470698ca3a24a6a
Victor-Sin <80609577+Victor-Sin@users.noreply.github.com>
2024-04-13 23:42:52 +0200

    Add pageContainer

--- src/app/components/joueurs/joueurs.jsx
+++ src/app/components/joueurs/joueurs.jsx
@@ -1,17 +1,11 @@
 import styles from "./joueurs.module.scss";
-import PlayersList from "./playersList/playersList";
-import { useState } from "react";
+import PageContainer from "../pageContainer/pageContainer";
 import AddPlayer from "./addPlayer/addPlayer";
 
 export default function Joueurs({ nextPage }) {
   return (
-    <main className={styles.container}>
-      <div className={styles.titleContent}>
-        <h1>
-          Tableau de bord des <span>joueurs</span>
-        </h1>
-      </div>
+    <PageContainer pageCategory={"player"}>
       <AddPlayer nextPage={nextPage}></AddPlayer>
-    </main>
+    </PageContainer>
   );
 }
---
+++ src/app/components/pageContainer/pageContainer.jsx
@@ -0,0 +1,94 @@
+import styles from "./pageContainer.module.scss";
+import { useSelector } from "react-redux";
+
+export default function PageContainer({
+  children,
+  pageCategory,
+  color = null,
+}) {
+  const players = useSelector((state) => state.players.players);
+  const currentBluffeur = useSelector((state) => state.players.currentBluffeur);
+
+  const colorStyle =
+    currentBluffeur != "" ? players[currentBluffeur].color : "";
+
+  const pageContent = () => {
+    switch (pageCategory) {
+      case "player":
+        return {
+          subTitle: "Sélectionner votre pion",
+          textureShader: "/image/players.png",
+          content: (
+            <p>
+              Pour lever le voile sur le passé de ceux que vous pensiez
+              connaître,
+              <br />
+              il vous faudra <b>entre 3 et 7</b> valeureux joueurs ou équipes.
+              <br />
+              Cliquez vite sur l’une des balles sombres et découvrez votre
+              couleur Joueur.
+            </p>
+          ),
+        };
+      case "remember":
+        return {
+          subTitle: "À présent,",
+          textureShader: "/image/remember.png",
+          content: (
+            <p>
+              Vite, vite !<br />
+              Vous n’avez que <b>1 min</b> pour piocher{" "}
+              <b
+                style={{
+                  color: "#373FEF",
+                }}
+              >
+                entre 1 et 3 cartes
+              </b>{" "}
+              !
+              <br />
+              Servez-vous de l’une d’entre elles pour vous remémorer le souvenir
+              que vous souhaitez partager.
+            </p>
+          ),
+        };
+      case "bluffer":
+        return {
+          subTitle: "Découvrez le",
+          textureShader: "/image/bluff.png",
+          content: (
+            <p>
+              Oh, c’est toi{" "}
+              <b style={{ color: colorStyle, textTransform: "capitalize" }}>
+                {currentBluffer}
+              </b>{" "}
+              ! Te voilà désigné comme le bluffer.
+              <br />
+              Scanne vite le QR code avec ton portable et isole toi...
+              <br />
+              le temps t’es compté !
+            </p>
+          ),
+        };
+      case "score":
+        return {
+          subTitle: "Voici le",
+          textureShader: "/image/score.png",
+          content: <p></p>,
+        };
+    }
+  };
+
+  return (
+    <main className={styles.container}>
+      <section className={styles.content}>
+        <div className={styles.titleContent}>
+          <h3>{pageContent().subTitle}</h3>
+          <img src={pageContent().textureShader} alt="" />
+        </div>
+        {pageContent().content}
+      </section>
+      {children}
+    </main>
+  );
+}
