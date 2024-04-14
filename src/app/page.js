4429338d7377e872f2ade2486e6499f7a404c3e8
Nicolas Neveu <nicolas.neveu@gmail.com>
2024-04-14 18:27:57 +0200

    up

--- src/app/page.js
+++ src/app/page.js
@@ -31,35 +31,36 @@ export default function Home() {
     };
 
     // Add event listeners
-    const hoverElem = hoverItemRef.current;
-    hoverElem.addEventListener("mouseenter", enterAnimation);
-    hoverElem.addEventListener("mouseleave", leaveAnimation);
+    // const hoverElem = hoverItemRef.current;
+    // hoverElem.addEventListener("mouseenter", enterAnimation);
+    // hoverElem.addEventListener("mouseleave", leaveAnimation);
 
-    // Cleanup the event listeners on component unmount
-    return () => {
-      hoverElem.removeEventListener("mouseenter", enterAnimation);
-      hoverElem.removeEventListener("mouseleave", leaveAnimation);
-    };
+    // // Cleanup the event listeners on component unmount
+    // return () => {
+    //   hoverElem.removeEventListener("mouseenter", enterAnimation);
+    //   hoverElem.removeEventListener("mouseleave", leaveAnimation);
+    // };
   }, []);
 
   return (
     <main className={styles.main}>
       <div className={styles.container}>
         <img src="/Logo.svg" alt="Logo" />
-        <div className={styles.btn}>
-          <svg ref={blobRef} id="svg" viewBox="0 0 1000 1000">
+        <div><a className={styles.btn} href="/intro">Jouer</a></div>
+        <div className={styles.blob}>
+        <svg ref={blobRef} id="svg" viewBox="0 0 1000 1000">
             <Blob
-              numPoints={8}
-              centerX={500}
-              centerY={500}
-              minRadius={400}
-              maxRadius={425}
+              numPoints={7}
+              centerX={50}
+              centerY={200}
+              minRadius={40}
+              maxRadius={42}
               minDuration={1}
               maxDuration={2}
             />
           </svg>
-          <p ref={hoverItemRef}>Jouer</p>
         </div>
+
       </div>
     </main>
   );
