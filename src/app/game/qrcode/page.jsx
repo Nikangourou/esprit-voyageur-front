c1bd13875ae1ee5caf3249723d150d410adcaec5
Nicolas Neveu <nicolas.neveu06@gmail.com>
2024-04-17 16:19:24 +0200

    fix deploy

--- src/app/game/qrcode/page.jsx
+++ src/app/game/qrcode/page.jsx
@@ -4,18 +4,17 @@ import styles from "./page.module.scss";
 import QrCode from "../../components/qrCode/qrCode";
 import PageContainer from "../../components/pageContainer/pageContainer";
 
-import { useContext, useEffect, useRef } from "react";
+import { useContext, useEffect, useRef, Suspense } from "react";
 import { useParams, useRouter } from "next/navigation";
 import { SocketContext } from "../../context/socketContext";
-// import { useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 
 export default function Code() {
   const router = useRouter();
   const isReady = useRef(false);
   const { socket } = useContext(SocketContext);
-  // const searchParams = useSearchParams();
-  // const gameId = searchParams.get("gameId");
-  const {gameId} = router.query
+  const searchParams = useSearchParams();
+  const gameId = searchParams.get("gameId");
 
   useEffect(() => {
     if (socket && !isReady.current) {
@@ -37,12 +36,14 @@ export default function Code() {
   }, [socket, gameId]);
 
   return (
-    <main className={styles.main}>
-      <div className={styles.container}>
-        <PageContainer pageCategory={"bluffer"}>
-          <QrCode gameId={gameId} />
-        </PageContainer>
-      </div>
-    </main>
+    <Suspense fallback={<div>Loading...</div>}>
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <PageContainer pageCategory={"bluffer"}>
+            <QrCode gameId={gameId} />
+          </PageContainer>
+        </div>
+      </main>
+    </Suspense>
   );
 }
