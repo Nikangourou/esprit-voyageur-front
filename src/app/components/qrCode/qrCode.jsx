fe5d58f452fb3856ba8b8197f1f01f3d7339cb5a
Victor-Sin <80609577+Victor-Sin@users.noreply.github.com>
2024-04-18 16:04:14 +0200

    Return to localhost

--- src/app/components/qrCode/qrCode.jsx
+++ src/app/components/qrCode/qrCode.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom";
 import { QRCodeSVG } from "qrcode.react";
 
 export default function QrCode({ gameId }) {
-  const url = `http://10.137.98.39:3000/voyageur?gameId=${gameId}`;
+  const url = `http://localhost:3000/voyageur?gameId=${gameId}`;
   console.log(url);
 
   return (
