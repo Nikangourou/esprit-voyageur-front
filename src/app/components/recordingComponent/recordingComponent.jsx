0870869d9b8da5117b89fdd1c0c699571724f5f2
Nicolas Neveu <nicolas.neveu06@gmail.com>
2024-04-18 17:42:16 +0200

    fix audio

--- src/app/components/recordingComponent/recordingComponent.jsx
+++ src/app/components/recordingComponent/recordingComponent.jsx
@@ -3,37 +3,35 @@ import styles from "./recording.module.scss";
 
 const RecordingComponent = ({ onEnd }) => {
   const [isRecording, setIsRecording] = useState(false);
-  // const [mediaRecorder, setMediaRecorder] = useState(null);
-
   const mediaRecorder = useRef(null);
 
   const initMediaRecorder = async () => {
-    if (
-      !mediaRecorder.current &&
-      navigator.mediaDevices &&
-      navigator.mediaDevices.getUserMedia
-    ) {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          audio: true,
-        });
-        const newMediaRecorder = new MediaRecorder(stream);
-        newMediaRecorder.ondataavailable = handleDataAvailable;
-        newMediaRecorder.onstop = () => setIsRecording(false);
-        newMediaRecorder.onerror = (event) =>
-          console.error("MediaRecorder error:", event.error);
 
-        // setMediaRecorder(newMediaRecorder);
-        mediaRecorder.current = newMediaRecorder;
-      } catch (error) {
-        console.error("Error accessing media devices:", error);
-      }
+    console.log(navigator);
+
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+      console.error("Media devices support is not available, (maybe you are not using https)");
+      return;
+    }
+
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+      });
+      const newMediaRecorder = new MediaRecorder(stream);
+      newMediaRecorder.ondataavailable = handleDataAvailable;
+      newMediaRecorder.onstop = () => {
+        setIsRecording(false);
+        // stream.getTracks().forEach((track) => track.stop());
+      };
+      newMediaRecorder.onerror = (event) =>
+        console.error("MediaRecorder error:", event.error);
+      mediaRecorder.current = newMediaRecorder;
+      startRecording();
+    } catch (error) {
+      console.error("Error accessing media devices:", error);
     }
   };
-
-  useEffect(() => {
-    initMediaRecorder();
-  }, []);
 
   const handleDataAvailable = (e) => {
     if (e.data.size > 0 && onEnd) {
@@ -42,11 +40,15 @@ const RecordingComponent = ({ onEnd }) => {
   };
 
   const startRecording = () => {
+    // Initialize the MediaRecorder if it hasn't been initialized yet
+    if (!mediaRecorder.current) {
+      initMediaRecorder(); 
+      return; 
+    }
+
     if (mediaRecorder.current && mediaRecorder.current.state === "inactive") {
       mediaRecorder.current.start();
       setIsRecording(true);
-    } else {
-      console.error("MediaRecorder not initialized or already recording");
     }
   };
 
