f6fc44176e74817bfc0c5924387acdf8f8b369aa
Nicolas Neveu <nicolas.neveu06@gmail.com>
2024-04-18 11:23:28 +0200

    deploy

---
+++ src/app/components/qrCode/qrCodeHandler.js
@@ -0,0 +1,51 @@
+"use client";
+
+import styles from "./qrCodeHandler.module.scss";
+import QrCode from "../components/qrCode/qrCode";
+import PageContainer from "../pageContainer/pageContainer";
+
+import { useContext, useEffect, useRef} from "react";
+import { useParams, useRouter } from "next/navigation";
+import { SocketContext } from "../../context/socketContext";
+import { useSearchParams } from "next/navigation";
+import { useDispatch } from "react-redux";
+import { selectBlufferPlayer } from "../../store/reducers/playersReducer";
+
+export default function Code() {
+  const router = useRouter();
+  const isReady = useRef(false);
+  const { socket } = useContext(SocketContext);
+  const searchParams = useSearchParams();
+  const gameId = searchParams.get("gameId");
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (socket && !isReady.current) {
+      isReady.current = true;
+      socket.emit("connexionPrimary", gameId);
+      dispatch(selectBlufferPlayer());
+    }
+    const handleNextPage = () => {
+      console.log("tewt");
+      // Rediriger vers la page suivante avec le gameId en paramètre
+      router.push(`/game?gameId=${gameId}`);
+    };
+
+    socket.on("startChrono", handleNextPage);
+    console.log("on");
+    return () => {
+      console.log("socketClose");
+      socket.off("startChrono", handleNextPage);
+    };
+  }, [socket, gameId]);
+
+  return (
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <PageContainer pageCategory={"bluffer"}>
+            <QrCode gameId={gameId} />
+          </PageContainer>
+        </div>
+      </main>
+  );
+}
--- src/app/game/qrcode/page.jsx
+++ src/app/game/qrcode/page.jsx
@@ -1,53 +1,14 @@
 "use client";
 
-import styles from "./page.module.scss";
-import QrCode from "../../components/qrCode/qrCode";
-import PageContainer from "../../components/pageContainer/pageContainer";
+import QrCodeHandler from "../../components/qrCode/qrHandler";
 
-import { useContext, useEffect, useRef, Suspense } from "react";
-import { useParams, useRouter } from "next/navigation";
-import { SocketContext } from "../../context/socketContext";
-import { useSearchParams } from "next/navigation";
-import { useDispatch } from "react-redux";
-import { selectBlufferPlayer } from "../../store/reducers/playersReducer";
+import {Suspense} from "react";
 
 export default function Code() {
-  const router = useRouter();
-  const isReady = useRef(false);
-  const { socket } = useContext(SocketContext);
-  const searchParams = useSearchParams();
-  const gameId = searchParams.get("gameId");
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    if (socket && !isReady.current) {
-      isReady.current = true;
-      socket.emit("connexionPrimary", gameId);
-      dispatch(selectBlufferPlayer());
-    }
-    const handleNextPage = () => {
-      console.log("tewt");
-      // Rediriger vers la page suivante avec le gameId en paramètre
-      router.push(`/game?gameId=${gameId}`);
-    };
-
-    socket.on("startChrono", handleNextPage);
-    console.log("on");
-    return () => {
-      console.log("socketClose");
-      socket.off("startChrono", handleNextPage);
-    };
-  }, [socket, gameId]);
-
+ 
   return (
-    <main className={styles.main}>
-      <div className={styles.container}>
-        <PageContainer pageCategory={"bluffer"}>
-          <Suspense fallback={<div>Loading...</div>}>
-            <QrCode gameId={gameId} />
-          </Suspense>
-        </PageContainer>
-      </div>
-    </main>
+    <Suspense fallback={<div>Loading...</div>}>
+      <QrCodeHandler />
+    </Suspense>
   );
 }
