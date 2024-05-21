681fe024ca6ae05034e7ad26f6a874ccd2d8a646
Nicolas Neveu <nicolas.neveu06@gmail.com>
2024-05-22 01:46:07 +0200

    add anim

--- src/app/components/score/score.jsx
+++ src/app/components/score/score.jsx
@@ -3,22 +3,78 @@ import Countdown from "../chrono/countdown";
 import Button from "../button/button";
 import { useDispatch, useSelector } from "react-redux";
 import Link from "next/link";
-import { useContext, useMemo } from "react";
+import { useContext, useMemo, useRef, useLayoutEffect } from "react";
 import { SocketContext } from "../../context/socketContext";
 import { newRound } from "../../store/reducers/playersReducer";
 import Footer from "../footer/footer";
+import { gsap } from "gsap";
 
 export default function Score({ gameId }) {
   const { socket } = useContext(SocketContext);
   const playersInGame = useSelector((state) => state.players.playersInGame);
   const players = useSelector((state) => state.players.players);
   const dispatch = useDispatch();
+  const column2Ref = useRef();
+  const column1Ref = useRef();
+  const column3Ref = useRef();
+  const tlRef = useRef();
+
+  useLayoutEffect(() => {
+    if (
+      !tlRef.current &&
+      column1Ref.current &&
+      column2Ref.current &&
+      column3Ref.current
+    ) {
+      tlRef.current = gsap
+        .timeline()
+        .fromTo(
+          column3Ref.current,
+          { scale: 0.8, opacity: 0 },
+          { scale: 1, opacity: 1, duration: 0.5, ease: "back.out(2)" }
+        )
+        .fromTo(
+          column3Ref.current.children[0],
+          { y: 0 },
+          { y: -8, duration: 0.5, ease: "back.out(3)" },
+          "+=0"
+        )
+        .fromTo(
+          column2Ref.current,
+          { scale: 0.8, opacity: 0 },
+          { scale: 1, opacity: 1, duration: 0.5, ease: "back.out(2)" },
+          "+=0.5"
+        )
+        .fromTo(
+          column2Ref.current.children[0],
+          { y: 0 },
+          { y: -8, duration: 0.5, ease: "back.out(3)" },
+          "+=0"
+        )
+        .fromTo(
+          column1Ref.current,
+          { scale: 0.8, opacity: 0 },
+          { scale: 1, opacity: 1, duration: 0.5, ease: "back.out(2)" },
+          "+=0.5"
+        )
+        .fromTo(
+          column1Ref.current.children[0],
+          { y: 0 },
+          { y: -8, duration: 0.5, ease: "back.out(3)" },
+          "+=0"
+        );
+    }
+  }, []);
 
   const sortedPlayersInGame = useMemo(() => {
     return [...playersInGame].sort(
       (a, b) => players[b].score - players[a].score
     );
   }, [playersInGame, players]);
+
+  const player1 = sortedPlayersInGame[0];
+  const player2 = sortedPlayersInGame[1];
+  const player3 = sortedPlayersInGame[2];
 
   function clickEvt(e) {
     dispatch(newRound());
@@ -31,22 +87,81 @@ export default function Score({ gameId }) {
         <div className={styles.scoreList}>
           {/* Podium for top 3 players */}
           <div className={styles.podium}>
-            {sortedPlayersInGame.slice(0, 3).map((playerId, i) => {
-              const player = players[playerId];
-              return (
-                <div
-                  className={`${styles.rank} ${styles[`rank${i + 1}`]}`}
-                  key={playerId}
-                >
-                  <Button
-                    type="player"
-                    color={player.color}
-                    colorActive={true}
-                  />
-                  <p>: {player.score}</p>
+            <div
+              ref={column2Ref}
+              className={`${styles.column} ${styles.column2}`}
+            >
+              <div className={styles.columnMain}>
+                <div>
+                  <p
+                    className={styles.rank}
+                    style={{
+                      filter: ` drop-shadow(2px 2px 0px ${players[player2].color})`,
+                    }}
+                  >
+                    2
+                  </p>
+                  <p
+                    className={styles.name}
+                    style={{ color: players[player2].color }}
+                  >
+                    {player2}
+                  </p>
                 </div>
-              );
-            })}
+                <p className={styles.score}>{players[player2].score} pts</p>
+              </div>
+              <div className={styles.sub}></div>
+            </div>
+            <div
+              ref={column1Ref}
+              className={`${styles.column} ${styles.column1}`}
+            >
+              <div className={styles.columnMain}>
+                <div>
+                  <p
+                    className={styles.rank}
+                    style={{
+                      filter: ` drop-shadow(2px 2px 0px ${players[player1].color})`,
+                    }}
+                  >
+                    1
+                  </p>
+                  <p
+                    className={styles.name}
+                    style={{ color: players[player1].color }}
+                  >
+                    {player1}
+                  </p>
+                </div>
+                <p className={styles.score}>{players[player1].score} pts</p>
+              </div>
+              <div className={styles.sub}></div>
+            </div>
+            <div
+              ref={column3Ref}
+              className={`${styles.column} ${styles.column3}`}
+            >
+              <div className={styles.columnMain}>
+                <div>
+                  <p
+                    className={styles.rank}
+                    style={{
+                      filter: ` drop-shadow(2px 2px 0px ${players[player3].color})`,
+                    }}
+                  >
+                    3
+                  </p>
+                  <p
+                    className={styles.name}
+                    style={{ color: players[player3].color }}
+                  >
+                    {player3}
+                  </p>
+                </div>
+                <p className={styles.score}>{players[player3].score} pts</p>
+              </div>
+              <div className={styles.sub}></div>
+            </div>
           </div>
 
           {/* Display the rest of the players */}
@@ -54,8 +169,20 @@ export default function Score({ gameId }) {
             const player = players[playerId];
             return (
               <div className={styles.row} key={playerId}>
-                <Button type="player" color={player.color} colorActive={true} />
-                <p>: {player.score}</p>
+                <div className={styles.flex}>
+                  <p
+                    className={styles.rank}
+                    style={{
+                      filter: ` drop-shadow(2px 2px 0px ${player.color})`,
+                    }}
+                  >
+                    {i + 4}
+                  </p>
+                  <p className={styles.name} style={{ color: player.color }}>
+                    {playerId}
+                  </p>
+                </div>
+                <p>{player.score} pts</p>
               </div>
             );
           })}
