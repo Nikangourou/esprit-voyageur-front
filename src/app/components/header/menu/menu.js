5bb72dd23320546a4d3e4f9fb265b5510db6ca36
Victor-Sin <80609577+Victor-Sin@users.noreply.github.com>
2024-06-26 11:35:49 +0200

    Add animation out menu

--- src/app/components/header/menu/menu.js
+++ src/app/components/header/menu/menu.js
@@ -1,16 +1,17 @@
 "use client";
 
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import styles from "./menu.module.scss";
 import { useRouter } from "next/navigation";
-import { newGame } from "../../../store/reducers/playersReducer";
 import { useDispatch } from "react-redux";
 import Galerie from "../galerie/galerie";
 import { SocketContext } from "../../../context/socketContext";
+import { gsap } from "gsap";
 
 export default function Menu({ openMenu, setOpenMenu }) {
   const router = useRouter();
   const dispatch = useDispatch();
+  const tlRef = useRef();
   const [showGalerie, setShowGalerie] = useState(false);
   const { setShouldResetGame: setShouldResetGame } = useContext(SocketContext);
 
@@ -18,12 +19,96 @@ export default function Menu({ openMenu, setOpenMenu }) {
     setShouldResetGame(true);
   };
 
+  useEffect(() => {
+    tlRef.current?.kill();
+    tlRef.current = gsap
+      .timeline()
+      .fromTo(
+        `.${styles.menu}`,
+        {
+          backdropFilter: "blur(0px)",
+          backgroundColor: "rgba(239, 235, 226, 0)",
+        },
+        {
+          backdropFilter: "blur(10px)",
+          backgroundColor: "rgba(239, 235, 226, 0.54)",
+          duration: 0.5,
+          ease: "power2.out",
+        },
+      )
+      .fromTo(
+        `.${styles.card}`,
+        { x: "100svw", y: "15svh", rotateZ: 25 },
+        {
+          x: 0,
+          y: 0,
+          rotateZ: 0,
+          duration: 1,
+          ease: "power2.out",
+          stagger: 0.25,
+        },
+        "<+.25",
+      )
+      .fromTo(
+        `.${styles.card} + p`,
+        { opacity: 0, y: "0.5rem", scale: 0.975 },
+        {
+          opacity: 1,
+          scale: 1,
+          y: 0,
+          duration: 0.75,
+          ease: "power4.out",
+          delay: 0.75,
+          stagger: 0.4,
+        },
+        "<",
+      );
+  }, []);
+
+  function animOut(onComplete) {
+    tlRef.current?.kill();
+    tlRef.current = gsap
+      .timeline({ onComplete: onComplete })
+      .to(`.${styles.card} + p`, {
+        opacity: 0,
+        duration: 0.75,
+        ease: "power4.out",
+      })
+      .to(
+        `.${styles.card}`,
+        {
+          x: "15svw",
+          y: "100svh",
+          rotateZ: 15,
+          duration: 1,
+          ease: "power2.out",
+          stagger: 0.25,
+        },
+        "<0.25",
+      )
+      .to(
+        `.${styles.menu}`,
+        {
+          backdropFilter: "blur(0px)",
+          backgroundColor: "rgba(239, 235, 226, 0)",
+          duration: 0.5,
+          ease: "power2.out",
+        },
+        "<+.5",
+      );
+  }
+
   return (
     <div className={styles.menu}>
       {showGalerie && <Galerie setShowGalerie={setShowGalerie} />}
       <div>
         <div
-          onClick={() => setOpenMenu(false)}
+          onClick={() => {
+            animOut(() => {
+              setOpenMenu(false);
+              console.log("aa");
+            });
+          }}
           className={`${styles.card} ${styles.continuer}`}
         >
           <div className={styles.containerImg}>
@@ -35,7 +120,9 @@ export default function Menu({ openMenu, setOpenMenu }) {
       <div>
         <div
           className={`${styles.card} ${styles.recommencer}`}
-          onClick={restartGame}
+          onClick={() => {
+            animOut(restartGame);
+          }}
         >
           <div className={styles.containerImg}>
             <img src="/images/quitter.svg" alt="logo" />
@@ -46,7 +133,9 @@ export default function Menu({ openMenu, setOpenMenu }) {
       <div>
         <div
           className={`${styles.card} ${styles.galerie}`}
-          onClick={() => setShowGalerie(true)}
+          onClick={() => {
+            setShowGalerie(true);
+          }}
         >
           <div className={styles.containerImg}>
             <img src="/images/galerie.svg" alt="logo" />
